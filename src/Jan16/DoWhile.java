8ca758e5a2c4a80e7d5b47dde2773e2b92638c67
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-01-16 21:12:27 -0500

    do while

---
+++ src/Jan16/DoWhile.java
@@ -0,0 +1,20 @@
+package Jan16;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<=10);
+
+    int j = 1;
+    int k = 5;
+        do {
+        System.out.println("5 * "+j+" = "+(j*k));
+        j++;
+    } while (j <= 10);
+
+
+
+}}
--- src/Jan16/table5Loops2.java
+++ src/Jan16/table5Loops2.java
@@ -1,15 +1,24 @@
 package Jan16;
 
+import java.util.Scanner;
+
 public class table5Loops2 {
     public static void main(String[] args) {
         int i = 1;
-        int j = 5;
-        while (i <= 10){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please find the number to find its factorial: ");
+        int j = sc.nextInt();
+        /*while (i <= 10){
             System.out.println("5 * "+i+" = "+(j*i));
             i++;
-        }
+        }*/
+        int factorial = 1;
+        while (i <= j){
+            factorial = factorial * j;
+            j--;
     }
-}
+        System.out.println("Your answer is: " +factorial);
+}}
 
 /*
 5 * 1 = 5
