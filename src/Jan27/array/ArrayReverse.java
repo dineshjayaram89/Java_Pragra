ac9518d6dda03ff9e3c5e071b2d4798131808bb1
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-01-27 14:02:13 -0500

    array reverse

---
+++ src/Jan27/array/ArrayReverse.java
@@ -0,0 +1,23 @@
+package Jan27.array;
+
+public class ArrayReverse {
+    public static void main(String[] args) {
+        int[] arr = {5,7,10,9,3,2};
+        for(int a : arr){
+            System.out.println(a);
+        }
+        System.out.println("The revered array is ");
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int k = arr[i];
+            arr[i] = arr[j];
+            arr[j] = k;
+            i++;
+            j--;
+        }
+        for(int b:arr){
+            System.out.println(b);
+        }
+    }
+}
