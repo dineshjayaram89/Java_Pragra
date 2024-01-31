06d5587639855e6882a2d7373e6d707105c24f61
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-01-30 20:51:26 -0500

    array programs

---
+++ src/Jan27/array/AverageOfArray.java
@@ -0,0 +1,22 @@
+package Jan27.array;
+
+import java.util.Scanner;
+
+public class AverageOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = {10,30,43,54,23,543,43,32};
+        double avg = average(array);
+        System.out.println("Sum of the array: " +avg);
+    }
+
+    public static double average(int arr[]){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        double avg = sum/arr.length;
+        return avg;
+    }
+
+}
