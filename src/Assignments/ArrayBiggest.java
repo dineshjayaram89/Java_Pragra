abfe3a195b117656976562c47ee8f849774099e3
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-01-30 20:52:37 -0500

    array assignments

---
+++ src/Assignments/ArrayBiggest.java
@@ -0,0 +1,31 @@
+package Assignments;
+
+public class ArrayBiggest {
+    public static void main(String[] args) {
+        int[] arr = {51, 17, 111, 921, 12333, 1122};
+        int big = 0;
+        System.out.print("The example array is: ");
+        for (int a : arr) {
+            System.out.println(a);
+        }
+
+        int i = 0;
+        int j = 1;
+        while (j < arr.length) {
+            int k = arr[i];
+            int l = arr[j];
+            if (k > l) {
+                if (k > big) {
+                    big = k;
+                }
+            } else if (k < l) {
+                if (l > big) {
+                    big = l;
+                }
+            }
+            i += 2;
+            j += 2;
+        }
+        System.out.println("The biggest number in the array is: " + big);
+    }
+}
---
+++ src/Assignments/SumOfArray.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = {10,30,43,54,23,543,43,32};
+        int sum = add(array);
+        System.out.println("Sum of the array: " +sum);
+    }
+
+    public static int add(int arr[]){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+                    }
+        return sum;
+    }
+
+
+}
