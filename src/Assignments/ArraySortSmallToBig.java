27db5d1aeccb212f9f24684c0c1bbf4e845f7225
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-02-03 09:15:09 -0500

    Sort

---
+++ src/Assignments/ArraySortSmallToBig.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class ArraySortSmallToBig {
+    public static void main(String[] args) {
+        int[] arr = {51, 17, 111, 921, 12333, 1122};
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for(int k : arr) {
+            System.out.println(k);
+        }
+    }
+}
