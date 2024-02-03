bbe4d4a0f6ef111085edb1372afd48b33c51ab17
dineshjayaram89 <149344575+dineshjayaram89@users.noreply.github.com>
2024-02-03 11:56:59 -0500

    StringMethod

---
+++ src/Feb3/StringConcat.java
@@ -0,0 +1,13 @@
+package Feb3;
+
+public class StringConcat {
+    public static void main(String[] args) {
+
+        String st1 = "Dinesh";
+        String st2 = "Jayaram";
+        String st3 = st1+" "+st2;
+        System.out.println(st3);
+        System.out.println(10+10+st3+(10+10)+10+10);
+
+    }
+}
---
+++ src/Feb3/StringMethod/StringMethod.java
@@ -0,0 +1,9 @@
+package Feb3.StringMethod;
+
+public class StringMethod {
+    public static void main(String[] args) {
+        Student student = new Student(10,"Dinesh");
+        System.out.println(student);
+    }
+
+}
